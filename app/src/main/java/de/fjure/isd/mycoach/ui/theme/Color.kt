d9fbb7cd6dca1af9ea24a242490c45a06955174a
fjure <45367873+fjure@users.noreply.github.com>
2022-05-30 00:52:11 +0200

    updated dependencies and added color scheme

--- app/src/main/java/de/fjure/isd/mycoach/ui/theme/Color.kt
+++ app/src/main/java/de/fjure/isd/mycoach/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package de.fjure.isd.mycoach.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+// Mainly used as background colors
+val DarkGrey = Color(0xFF404040)
+val Grey = Color(0xFF878787)
+val MistyBlue = Color(0xFF2F5061)
+val Coral = Color(0xFFE57F84)
+val Ivory = Color(0xFFF4EAE6)
+val TealGreen = Color(0xFF4297A0)
+
+// Used for Component Colors
+val Red = Color(0xFFFF5F66)
+val Orange = Color(0xFFFFA37E)
+val Blue = Color(0xFF00E7FF)
